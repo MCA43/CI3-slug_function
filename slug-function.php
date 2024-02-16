4a427a45f8859155b191c5467051caf8d42d2b46
Mücahit Cemal AY <74060611+MCA43@users.noreply.github.com>
2024-02-17 00:43:46 +0300

    Add files via upload

--- slug-function.php
+++ slug-function.php
@@ -3,13 +3,13 @@
     function isSlug($model = null, $column = null, $slug = null) {
         if (is_null($slug) || empty($slug) || is_null($model) || empty($model) || is_null($column) || empty($column)){ return false; }
         $slug = convertToSEO($slug);
-        $t = get_instance();
-        $isSlug = $t->$model->get([$column => $slug]);
+        $ci = get_instance();
+        $isSlug = $ci->$model->get([$column => $slug]);
         if ($isSlug){
             $say = 1;
             while ($isSlug == true){
                 $new_slug = $slug."-".$say++;
-                $isSlug = $t->$model->get([$column => $new_slug]);
+                $isSlug = $ci->$model->get([$column => $new_slug]);
                 if ($isSlug){
                     $new_slug = $slug;
                 }else{
@@ -27,24 +27,21 @@
 
 <?php 
 
-    // Yapay Zekanın Revize Ettiği Kod
-    function generate_unique_slug($table = null, $slug_column = null, $input_slug = null) {
-        if (empty($table) || empty($slug_column) || empty($input_slug)) { return false; }
-        $input_slug = convertToSEO($input_slug);
-        $ci = get_instance();
-        $all_slugs = $ci->$table->get_all($slug_column);
-        $slug_array = array_column($all_slugs, $slug_column);
-        $exists = in_array($input_slug, $slug_array);
-        if ($exists) {
-            $count = 0;
-            while ($exists) {
-                $output_slug = $input_slug."-".++$count;
-                $exists = in_array($output_slug, $slug_array);
-            }
-            return $output_slug;
-        } else {
-            return $input_slug;
+    // Revize Edilen Kod
+    function isSlug($table = null, $slug_column = null, $input_slug = null) {
+    $ci = get_instance();
+    $existingSeflinks = $ci->$table->get_all();
+    $desiredSeflink = convertToSEO($input_slug);
+
+    if (in_array($desiredSeflink, array_column($existingSeflinks, $slug_column)) === true) {
+        $i = 1;
+        while (in_array($desiredSeflink . '-' . $i, array_column($existingSeflinks, $slug_column))) {
+            $i++;
         }
+        return $desiredSeflink. '-' . $i; die();
+    } else {
+        return $desiredSeflink; die();
     }
+}
 
 ?>
